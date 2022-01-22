401331048aeb27a589fadf4d7fcc1227c7ec178d
Мамытов Руслан <rmamytov98@gmail.com>
2022-01-22 18:21:47 +0700

    Working internet monitoring but needs refactoring

--- app/src/main/java/com/example/newsapp/util/NetworkStatusHelper.kt
+++ app/src/main/java/com/example/newsapp/util/NetworkStatusHelper.kt
@@ -2,9 +2,10 @@ package com.example.newsapp.util
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.ConnectivityManager.*
 import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
+import android.net.NetworkCapabilities.*
+import android.os.Build
 import android.util.Log
 import androidx.lifecycle.LiveData
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -22,81 +23,91 @@ sealed class NetworkStatus {
 class NetworkStatusHelper @Inject constructor(@ApplicationContext private val context: Context) :
     LiveData<NetworkStatus>() {
 
-    val validNetworkConnections: MutableSet<Network> = HashSet()
     private val connectivityManager: ConnectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    private lateinit var connectivityManagerCallback: ConnectivityManager.NetworkCallback
-
-    fun announceStatus() {
-        Log.d(TAG, "announceStatus: ${validNetworkConnections.size}")
-        if (validNetworkConnections.isNotEmpty()) {
-            postValue(NetworkStatus.Available)
-        } else {
-            postValue(NetworkStatus.Unavailable)
-        }
-    }
-
-    private fun getConnectivityManagerCallback() =
+    private val connectivityManagerCallback =
         object : ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: Network) {
                 super.onAvailable(network)
+                postValue(NetworkStatus.Available)
                 Log.d(TAG, "onAvailable: available")
-                val networkCapability = connectivityManager.getNetworkCapabilities(network)
-                val hasNetworkConnection =
-                    networkCapability?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                        ?: false
-                if (hasNetworkConnection) {
-                    validNetworkConnections.add(network)
-                    announceStatus()
-                }
             }
 
             override fun onUnavailable() {
                 super.onUnavailable()
                 Log.d(TAG, "onUnavailable: no connection")
-                announceStatus()
+                postValue(NetworkStatus.Unavailable)
             }
 
             override fun onLost(network: Network) {
                 super.onLost(network)
                 Log.d(TAG, "onLost: lost connection")
-                validNetworkConnections.remove(network)
-                announceStatus()
+                postValue(NetworkStatus.Unavailable)
             }
 
-            override fun onCapabilitiesChanged(
-                network: Network,
-                networkCapabilities: NetworkCapabilities
-            ) {
-                super.onCapabilitiesChanged(network, networkCapabilities)
-                Log.d(TAG, "onCapabilitiesChanged: changed type")
-                if (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
-                    validNetworkConnections.add(network)
-                } else {
-                    validNetworkConnections.remove(network)
-                }
-                announceStatus()
-            }
+            /* override fun onCapabilitiesChanged(
+                 network: Network,
+                 networkCapabilities: NetworkCapabilities
+             ) {
+                 super.onCapabilitiesChanged(network, networkCapabilities)
+                 Log.d(TAG, "onCapabilitiesChanged: changed type")
+                 if (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+                     validNetworkConnections.add(network)
+                 } else {
+                     validNetworkConnections.remove(network)
+                 }
+                 announceStatus()
+             }*/
         }
 
+    private fun checkConnection(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.activeNetworkInfo?.run {
+                return when (type) {
+                    TYPE_WIFI -> true
+                    TYPE_MOBILE -> true
+                    TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+        return false
+    }
 
     override fun onActive() {
         super.onActive()
-        Log.d(TAG, "onActive: started working")
-        connectivityManagerCallback = getConnectivityManagerCallback()
-        val networkRequest = NetworkRequest.Builder()
+        val hasInternet = checkConnection()
+        if (hasInternet) {
+            postValue(NetworkStatus.Available)
+        } else {
+            postValue(NetworkStatus.Unavailable)
+        }
+        /*val networkRequest = NetworkRequest.Builder()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
             .build()
-        connectivityManager.registerNetworkCallback(networkRequest, connectivityManagerCallback)
+        Log.d(
+            TAG,
+            "onActive: ${networkRequest.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)}"
+        )
+        connectivityManager.registerDefaultNetworkCallback(connectivityManagerCallback)
+        //connectivityManager.registerNetworkCallback(networkRequest, connectivityManagerCallback)*/
     }
 
     override fun onInactive() {
         super.onInactive()
         Log.d(TAG, "onInactive: stopped working")
-        connectivityManager.unregisterNetworkCallback(connectivityManagerCallback)
+        //connectivityManager.unregisterNetworkCallback(connectivityManagerCallback)
     }
-
 
 }
